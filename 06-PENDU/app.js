e6584dc54eeab120be270dfb2353c6c764711a44
Safia54 <sbihmedn@gmail.com>
2017-08-04 20:47:03 +0200

    pushe

--- 06-PENDU/app.js
+++ 06-PENDU/app.js
@@ -1,26 +1,42 @@
+
+//rappel sur les fonctions
 var ville = "Namur";
 const pays = "Belgique";
 let commune = "Ixelles";
 // pays = "France"; // pas bien
 
 function deplacer (){
-	var ville = "France";
+	var ville = "Liège";
 	console.log("Je me déplace vers " + ville);
 }
 deplacer();
 console.log(ville);
 
+
+
 //Exercice : jeu du pendu
+// essai Safia 1
 const MotADeviner = ['B','O','N','J','O','U','R'];
-console.log['MotADeviner' + MotADeviner]
+var TableauLettresProposees= ['','','','','','',''];
 
-var LettresProposees= ['','','','','','',''];
 
-function GuessLetter() {
-	for (let i = 0; i < MotADeviner.length; i++) {
-		if (x === MotADeviner[i]) {
-			LettresProposees.splice(i,1,GuessLetter())
-			//?????????????????????,,,,,,,, 	FAIRE CHEZ SOI A LA MAISON !!!!!
-		}
-	}
-}	
+
+
+if (TableauLettresProposees = MotADeviner) {
+	window.alert("Gagné ! T'as deviné le mot !");
+
+}
+// //debut commencé en classe
+// const MotADeviner = ['B','O','N','J','O','U','R'];
+// console.log['MotADeviner' + MotADeviner]
+
+// var LettresProposees= ['','','','','','',''];
+
+// function GuessLetter() {
+// 	for (let i = 0; i < MotADeviner.length; i++) {
+// 		if (x === MotADeviner[i]) {
+// 			LettresProposees.splice(i,1,GuessLetter())
+// 			//?????????????????????,,,,,,,, 	FAIRE CHEZ SOI A LA MAISON !!!!!
+// 		}
+// 	}
+// }	
--- 07-MATHS/app.js
+++ 07-MATHS/app.js
@@ -12,13 +12,13 @@ var max = Math.max(-3, 9, 21, 36, 27, 54, 17, 35);
 var addition = min + max;
 console.log(min,max,addition);
 
-//Exercice : Aléatoire n°1
+//Exercice : Aléatoire 1
 var floatBateau = 10.4;
 var couleBateau = Math.floor(floatBateau);
 var voleBateau = Math.ceil(floatBateau);
 console.log(floatBateau, couleBateau, voleBateau);
 
-//Exercice : Aléatoire n°2
+//Exercice : Aléatoire 2
 var aleatoire50_100 = Math.random() * 50 + 50; 
 
 var aleatoire0ou1 = Math.round(Math.random());
@@ -26,3 +26,9 @@ var aleatoire0ou1 = Math.round(Math.random());
 var aleatoire0_10inclu = Math.round(Math.random() * 10);
 
 console.log(aleatoire50_100, aleatoire0ou1, aleatoire0_10inclu);
+
+//Exercice : Aléatoire 3
+var mortalKombat = ["Goro", "Johnny Cago", "Kano", "Liu Kano", "Raiden", "Reptil", "Scorpion", "Shang Tsun", "Sonya", "Sub-Zero"];
+var personnage = mortalKombat[Math.round(Math.random() * mortalKombat.length-1)];
+console.log(mortalKombat); 
+console.log(personnage);
